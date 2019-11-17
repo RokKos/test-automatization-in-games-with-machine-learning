55ed3cb54488e3e4c236e41ef490bda50a7e0e62
RokKos <rocki5555@gmail.com>
2019-11-17 18:35:07 +0100

    Camera position and scale improved

--- Unity/i_am_here/Assets/Code/IAmHere.Game/Camera/MainCameraController.cs
+++ Unity/i_am_here/Assets/Code/IAmHere.Game/Camera/MainCameraController.cs
@@ -1,12 +1,16 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 
 namespace IAmHere.Game
 {
     public class MainCameraController : MonoBehaviour
     {
+        [SerializeField] private Camera mainCamera = null;
         public void Init(int levelColumns, int levelRows)
         {
-            transform.position = new Vector3((float) levelColumns / 2, -(float) levelRows / 2, transform.position.z);
+            transform.position = new Vector3((float) levelColumns / 2, -(float) levelRows / 2 + 0.5f, transform.position.z);
+            int longerAxis = Math.Max(levelColumns, levelRows);
+            mainCamera.orthographicSize = (float) longerAxis / 2;
         }
 
     }
