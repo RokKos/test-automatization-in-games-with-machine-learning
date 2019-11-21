4d33cb4a61158cbcad84c25c29000bc8b2967f8b
RokKos <rocki5555@gmail.com>
2019-11-21 20:16:31 +0100

    Abstracted out player controller

--- Unity/i_am_here/Assets/Code/IAmHere.Game/Player/PlayerController.cs
+++ Unity/i_am_here/Assets/Code/IAmHere.Game/Player/PlayerController.cs
@@ -2,21 +2,9 @@
 
 namespace IAmHere.Game
 {
-    
-
-    public class PlayerController : MovingEntityController
+    public class PlayerController : ControlableEntityController
     {
-
-        public delegate void OnPlayerDead(WorldEntityController killingEntity);
-        public OnPlayerDead onPlayerDead;
         
-        public delegate void OnLevelClear();
-        public OnLevelClear onLevelClear;
-        
-        
-        private bool playerDead = false;
-
-        // Update is called once per frame
         void Update() {
 
             if (state == MovingState.kMoved && burstTimer > nextBurstTime)
@@ -33,49 +21,29 @@ namespace IAmHere.Game
 
             burstTimer += Time.deltaTime;
 
-
+            Vector2 dir = Vector2.zero;
             // Input handling
             if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
             {
-                transform.position += Vector3.left * movementCoeficient;
-                state = MovingState.kMoved;
+                dir.x += -1;
             }
 
             if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
             {
-                transform.position += Vector3.right * movementCoeficient;
-                state = MovingState.kMoved;
+                dir.x += 1;
             }
 
             if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow))
             {
-                transform.position += Vector3.up * movementCoeficient;
-                state = MovingState.kMoved;
+                dir.y += 1;
             }
 
             if (Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.DownArrow))
             {
-                transform.position += Vector3.down * movementCoeficient;
-                state = MovingState.kMoved;
+                dir.y += -1;
             }
-        }
-
-        protected override void OnCollisionEnter2D(Collision2D other)
-        {
-            if (other.gameObject.tag == "Goal")
-            {
-                onLevelClear();
-                return;
-            }
-
-
-            if (other.gameObject.tag == "Enviroment")
-            {
-                // TODO(Rok Kos): Create action to game manager that is game over...
-                playerDead = true;
-                onPlayerDead(other.gameObject.GetComponent<WorldEntityController>());
-                state = MovingState.kHurt;
-            }
+            
+            MoveEntity(dir);
         }
     }
 }
---
+++ Unity/i_am_here/Assets/Code/IAmHere.Game/WorldObjects/ControlableEntityController.cs
@@ -0,0 +1,55 @@
+﻿
+using UnityEngine;
+
+namespace IAmHere.Game
+{
+    public class ControlableEntityController : MovingEntityController
+    {
+        public delegate void OnPlayerDead(WorldEntityController killingEntity);
+        public OnPlayerDead onPlayerDead;
+        
+        public delegate void OnLevelClear();
+        public OnLevelClear onLevelClear;
+        
+        
+        protected bool playerDead = false;
+        protected bool playerWon = false;
+        
+        public void Init()
+        {
+            base.Init();
+            playerDead = false;
+            playerWon = false;
+        }
+        
+        protected override void OnCollisionEnter2D(Collision2D other)
+        {
+            if (other.gameObject.tag == "Goal")
+            {
+                onLevelClear();
+                playerWon = true;
+                return;
+            }
+
+
+            if (other.gameObject.tag == "Enviroment")
+            {
+                playerDead = true;
+                onPlayerDead(other.gameObject.GetComponent<WorldEntityController>());
+                state = MovingState.kHurt;
+            }
+        }
+
+        protected void MoveEntity(Vector2 dir)
+        {
+            if (dir != Vector2.zero)
+            {
+                state = MovingState.kMoved;
+            }
+            
+            transform.position += new Vector3(dir.x, dir.y, 0) * movementCoeficient;
+            
+        }
+
+    }
+}
