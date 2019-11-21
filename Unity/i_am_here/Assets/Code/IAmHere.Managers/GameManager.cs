5374930a5ae8c0525dadb0ac02c7183c598f18de
RokKos <rocki5555@gmail.com>
2019-11-21 07:28:03 +0100

    Singelton pattern and first manager

---
+++ Unity/i_am_here/Assets/Code/IAmHere.Managers/GameManager.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using UnityEngine;
+using IAmHere.Utilities;
+using IAmHere.WorldGeneration;
+
+
+namespace IAmHere.Managers
+{
+    public class GameManager : Singleton<GameManager>
+    {
+        public static WorldManager WorldManager = null;
+
+        private void Start()
+        {
+            // TODO(Rok Kos): Rethink this decision
+            WorldManager = FindObjectOfType<WorldManager>();
+        }
+    }
+}
